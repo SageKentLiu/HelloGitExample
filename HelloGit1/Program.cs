9179fc2df52231c25e3df849e51f6c3b5b3c5dd9
Kent Liu <kent.liu@sage.com>
2013-04-02 16:31:15 -0400

    initial Commit

---
+++ HelloGit1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloGit1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kent");
+            Console.ReadKey();
+        }
+    }
+}
