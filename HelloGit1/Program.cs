f66c20fb67ff6183f483aaffa37e48ac7774d8c8
Kent Liu <kent.liu@sage.com>
2013-04-04 15:13:41 -0400

    add code

--- HelloGit1/Program.cs
+++ HelloGit1/Program.cs
@@ -9,9 +9,40 @@ namespace HelloGit1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("kent");
+            double da = 1;
+            DateTime dt = new DateTime(2013,4, 2);
+            DateTime dt1 = new DateTime(2013, 6, 20);
+            da = (dt - dt1).TotalDays / 2 + 1;
+            dt = dt.AddDays(da);
+            short d = (short)dt.DayOfWeek;
             Console.WriteLine("kent1");
+            Console.WriteLine("GetForture : " + GetForture());
             Console.ReadKey();
         }
+
+        private static string GetForture()
+        {
+            Random rnd = new Random();
+            int whichone = rnd.Next(3);
+            string tmp = "";
+            switch (whichone)
+            {
+                case 1:
+                    tmp  = "1";
+                    break;
+                case 2:
+                    tmp =  "2";
+                    break;
+                case 3:
+                    tmp = "3";
+                    break;
+                default:
+                    tmp = "default";
+                    break;
+
+            }
+            return tmp;
+        }
+        
     }
 }
